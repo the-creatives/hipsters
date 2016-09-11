0e54f57a93a016f4511dfae542a86f6096af01c3
James W <james.wu@alumni.olin.edu>
2016-09-11 00:15:50 -0400

    boilerplate templates

--- hipstersapp/hipstersapp/settings.py
+++ hipstersapp/hipstersapp/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+ABS_TEMPLATES_PATH = '%s/templates' % BASE_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'hipstersapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ABS_TEMPLATES_PATH],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
