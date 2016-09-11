4a57a151c79c3e52ceaede64db9e9bd4392da2df
James W <james.wu@alumni.olin.edu>
2016-09-11 00:27:46 -0400

    hook up our app to our hipsters collection

--- hipstersapp/hipstersapp/homeview.py
+++ hipstersapp/hipstersapp/homeview.py
@@ -1,7 +1,9 @@
 from django.views.generic import TemplateView
 import indicoio
 import indico
+from indicoio.custom import Collection
 
+hipsterv2 = Collection("Hipster V2")
 indicoio.config.api_key = indico.INDICO_KEY
 
 class HomeView(TemplateView):
@@ -9,6 +11,6 @@ class HomeView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         context = {
-            'some_dynamic_value': 'This text comes from django view!',
+            'some_dynamic_value': "Some value",
         }
         return self.render_to_response(context)
